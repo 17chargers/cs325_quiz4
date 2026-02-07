199dced970ba874bd620bda2b57081cd75ffc4d7
Nathan-Clas5 <Nathanbc26@gmail.com>
2026-02-07 14:22:32 -0600

    Mult calculator.py
    Basic multiplication functions added.

--- CS325_Quiz4/calculator.py
+++ CS325_Quiz4/calculator.py
@@ -1,1 +1,9 @@
+def multiply(a, b):
+  return a * b
 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = multiply(num1, num2)
+
+print(f"{num1} times {num2} equals {result}")
